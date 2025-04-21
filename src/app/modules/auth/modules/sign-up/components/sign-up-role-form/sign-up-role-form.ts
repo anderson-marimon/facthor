b1d41a858541f22a49f35ae09320c3f70075a460
Anderson Marimon <anderson.marimon@silvertec.com.co>
2025-04-21 11:19:32 -0500

    feat [button-component]: refactored button component and updated layout styles for sign-up forms
    - Deleted old button directive and replaced it with the new FrsButton component.
    - Created new FrsButton component for improved button functionality and styling.
    - Renamed button module to frs-button and updated references accordingly.
    - Updated layout styles in sign-up business form, role card, and role form components.
    - Modified sign-up-related HTML and TS files to integrate the new FrsButton component and update layout styles.

--- src/app/modules/auth/modules/sign-up/components/sign-up-role-form/sign-up-role-form.ts
+++ src/app/modules/auth/modules/sign-up/components/sign-up-role-form/sign-up-role-form.ts
@@ -1,59 +1,66 @@
-import { type AfterViewInit, ChangeDetectionStrategy, Component, DestroyRef, inject, viewChildren } from '@angular/core';
+import {
+    type AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    DestroyRef,
+    inject,
+    viewChildren,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, type FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { FrsButtonModule } from '@fresco-ui/button/button.module';
 import { FinancierLogo } from '@shared/logos/financier-logo/financier-logo';
 import { PayerLogo } from '@shared/logos/payer-logo/payer-logo';
 import { ProviderLogo } from '@shared/logos/provider-logo/provider-logo';
 import { SignUpRoleCard } from '../sign-up-role-card/sign-up-role-card';
+import { FrsButtonModule } from '@fresco-ui/frs-button';
 
 @Component({
-	selector: 'sign-up-role-form',
-	templateUrl: 'sign-up-role-form.html',
-	host: { 'class': 'flex-1' },
-	imports: [FrsButtonModule, ReactiveFormsModule, SignUpRoleCard, FinancierLogo, ProviderLogo, PayerLogo],
-	changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'sign-up-role-form',
+    templateUrl: 'sign-up-role-form.html',
+    host: { class: 'flex-1' },
+    imports: [FrsButtonModule, ReactiveFormsModule, SignUpRoleCard, FinancierLogo, ProviderLogo, PayerLogo],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignUpRoleForm implements AfterViewInit {
-	private readonly _formBuilder = inject(FormBuilder);
-	private readonly _destroyRef = inject(DestroyRef);
-	private readonly _roleCard = viewChildren(SignUpRoleCard);
+    private readonly _formBuilder = inject(FormBuilder);
+    private readonly _destroyRef = inject(DestroyRef);
+    private readonly _roleCard = viewChildren(SignUpRoleCard);
 
-	public readonly inputRole = this._formBuilder.control('', Validators.required);
+    public readonly inputRole = this._formBuilder.control('', Validators.required);
 
-	protected readonly _roleForm = this._formBuilder.group({ option: this.inputRole });
+    protected readonly _roleForm = this._formBuilder.group({ option: this.inputRole });
 
-	private _onSelectRole(): void {
-		this._roleForm.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(({ option }) => {
-			this._setSelectedRole(option || '0');
-		});
-	}
+    private _onSelectRole(): void {
+        this._roleForm.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(({ option }) => {
+            this._setSelectedRole(option || '0');
+        });
+    }
 
-	private _setSelectedRole(selectedRole: string): void {
-		const roles = this._roleCard();
-		if (roles.length === 0) return;
+    private _setSelectedRole(selectedRole: string): void {
+        const roles = this._roleCard();
+        if (roles.length === 0) return;
 
-		roles.forEach(role => {
-			if (role.value() === selectedRole) {
-				return role.setSelected(true);
-			}
+        roles.forEach(role => {
+            if (role.value() === selectedRole) {
+                return role.setSelected(true);
+            }
 
-			role.setSelected(false);
-		});
-	}
+            role.setSelected(false);
+        });
+    }
 
-	protected _onSubmit() {
-		if (this._roleForm.valid) {
-			const selectedOption = this.inputRole.value;
-			console.log('Opción seleccionada:', selectedOption);
-		}
-	}
+    protected _onSubmit() {
+        if (this._roleForm.valid) {
+            const selectedOption = this.inputRole.value;
+            console.log('Opción seleccionada:', selectedOption);
+        }
+    }
 
-	public getRoleForm(): FormGroup {
-		return this._roleForm;
-	}
+    public getRoleForm(): FormGroup {
+        return this._roleForm;
+    }
 
-	public ngAfterViewInit(): void {
-		this._onSelectRole();
-	}
+    public ngAfterViewInit(): void {
+        this._onSelectRole();
+    }
 }
