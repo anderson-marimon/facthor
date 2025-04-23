7db5d33fc9c500f1acbcbb19fa7dcb30aeb95007
Anderson Marimon <anderson.marimon@silvertec.com.co>
2025-04-23 11:39:32 -0500

    feat [environment]: added environment variables and updated configuration for development and production
    - Created .env-template for environment variables configuration.
    - Added environment variable files: src/envs/envs.development.ts and src/envs/envs.ts.
    - Updated package.json, angular.json, and pnpm-lock.yaml for environment integration.
    - Introduced TypeScript type definitions in src/env.d.ts for environment variables.
    - Modified .gitignore to exclude environment-specific files.

---
+++ src/envs/envs.development.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  producction: false,
+  url: import.meta.env.FT_URL_LOGIN,
+  path_auth: import.meta.env.FT_AUTH_PATH,
+  path_config_user: import.meta.env.FT_AUTH_CONFIG_USER,
+
+  urn: import.meta.env.FT_URN,
+
+  url_register: import.meta.env.FT_URL_REGISTER,
+  path_register: import.meta.env.FT_REGISTER_PATH,
+
+  URL_CLIENT_UPLOAD: import.meta.env.FT_URL_CLIENT_UPLOAD,
+  URL_NEGOTIATION: import.meta.env.FT_NEGOTIATION,
+
+  PATH_AUTH_CHANGE_PASSWORD: import.meta.env.FT_AUTH_CHANGE_PASSWORD,
+  PATH_AUTH_FORGOT_PASSWORD: import.meta.env.FT_AUTH_FORGOT_PASSWORD,
+
+  PATH_REGISTER_VERIFY_TOKEN: import.meta.env.FT_REGISTER_VERIFY_TOKEN,
+  PATH_REGISTER_LIST_DOCUMENT: import.meta.env.FT_REGISTER_LIST_DOCUMENT,
+  PATH_REGISTER_RENEW_UPDATE_FILES_TOKEN : import.meta.env.FT_REGISTER_RENEW_UPDATE_FILES_TOKEN,
+  PATH_REGISTER_UPDATE_FAILED_LEGAL_DOCUMENT : import.meta.env.FT_REGISTER_UPDATE_FAILED_LEGAL_DOCUMENT,
+
+  PATH_URL_FINANCIER_PROVIDER: import.meta.env.FT_URL_FINANCIER_PROVIDER,
+};
