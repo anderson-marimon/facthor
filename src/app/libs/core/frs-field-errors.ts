2943651f7136c5486160aba9cfd7a170b0c026f7
Anderson Marimon <anderson.marimon@silvertec.com.co>
2025-04-29 15:05:25 -0500

    feat [auth-sign-up]: update password field validation and add eye toggle for visibility

--- src/app/libs/core/frs-field-errors.ts
+++ src/app/libs/core/frs-field-errors.ts
@@ -17,7 +17,7 @@ export const INPUT_ERRORS = {
 	invalidBankAccountNumber: 'Solo se permiten números',
 	pdfInvalidFileType: 'Solo se permiten archivos PDF',
 	invalidFileSizeTwoMb: 'El tamaño máximo permitido es de 2 MB',
-	invalidPasswordMinMax: 'La contraseña debe tener entre 7 y 10 caracteres',
+	invalidPasswordMinMax: 'Mínimo 8 - máximo 20 caracteres',
 	missingUpperCase: 'Debe contener al menos una letra mayúscula',
 	missingNumber: 'Debe contener al menos un número',
 	missingSpecialChar: 'Debe contener al menos un carácter especial',
